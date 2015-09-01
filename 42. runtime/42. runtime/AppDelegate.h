94333f7aed6cef3bc0019ad3d6e6ad380c75e928
jiaxianhua <jiaxianhua@gmail.com>
2015-09-01 23:15:16 +0800

    runtime

---
+++ 42. runtime/42. runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  42. runtime
+//
+//  Created by jiaxianhua on 15/9/1.
+//  Copyright (c) 2015年 jiaxianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 42. runtime/42. runtime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  42. runtime
+//
+//  Created by jiaxianhua on 15/9/1.
+//  Copyright (c) 2015年 jiaxianhua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
