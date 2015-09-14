c097f257a6d6f29e6d982ed556d1bf5d4a3c9d25
jiaxianhua <jiaxianhua@gmail.com>
2015-09-14 22:52:53 +0800

    New Project 2048

---
+++ 2048/2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by jiaxianhua on 15/9/14.
+//  Copyright © 2015年 jiaxianhua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
