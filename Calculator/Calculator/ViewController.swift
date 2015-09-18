c76207bbb17ff1a4441f7b4b03f91f41044debf9
jiaxianhua <jiaxianhua@gmail.com>
2015-09-18 23:46:59 +0800

    userIsInTheMiddleOfTypingANumber

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var userIsInTheMiddleOfTypingANumber: Bool = false
     @IBOutlet weak var display: UILabel!
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle
-        display.text = "\(digit)"
+        if userIsInTheMiddleOfTypingANumber {
+            
+            display.text = display.text! + "\(digit!)"
+        }
+        else {
+            display.text = "\(digit!)"
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
 }
 
