ff55cfe809423ecb896f27c00ca31204f88e2336
jiaxianhua <jiaxianhua@gmail.com>
2015-09-18 23:08:22 +0800

    Developing iOS 8 Apps with Swift Calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by jiaxianhua on 15/9/18.
+//  Copyright (c) 2015年 com.jiaxh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
