61de3008819873ed0a7b149c6e1572c2bf73be54
jiaxianhua <jiaxianhua@gmail.com>
2015-09-19 22:24:21 +0800

    remove "return"

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
         
         switch operation {
         case "×": performOperation({ (op1, op2) in
-            return op1 * op2
+            op1 * op2
         })
 //        case "÷":
 //        case "+":
