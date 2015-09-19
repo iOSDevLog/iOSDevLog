1554030a4562fa70cf080572b131c143832fd7e5
jiaxianhua <jiaxianhua@gmail.com>
2015-09-19 22:28:20 +0800

    remove " () "

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
         }
         
         switch operation {
-        case "×": performOperation() { $0 * $1 }
+        case "×": performOperation { $0 * $1 }
 //        case "÷":
 //        case "+":
 //        case "−":
