5d9d3a07c2665fae768efe64aba3ea7da761cc86
jiaxianhua <jiaxianhua@gmail.com>
2015-09-20 21:54:53 +0800

    Printable

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -9,10 +9,23 @@
 import Foundation
 
 class CalculatorBrain {
-    enum Op {
+    enum Op: Printable {
         case Operand(Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
     }
     
     private var opStack = [Op]()
