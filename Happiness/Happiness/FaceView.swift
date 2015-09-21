ca64ee80f82650c885a3e8e1e681590934edadd9
jiaxianhua <jiaxianhua@gmail.com>
2015-09-21 23:23:57 +0800

    FaceView

---
+++ Happiness/Happiness/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by jiaxianhua on 15/9/21.
+//  Copyright © 2015年 com.jiaxh. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
