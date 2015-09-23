b026fa8c9e5e1513d1ec89e851e1adb6d6840fd0
jiaxianhua <jiaxianhua@gmail.com>
2015-09-23 22:10:25 +0800

    New Project

---
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by jiaxianhua on 15/9/23.
+//  Copyright © 2015年 com.jiaxh. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
