6b03a528d76afd24f94776b1f3354a30b4982fec
jiaxianhua <jiaxianhua@gmail.com>
2015-09-23 22:10:41 +0800

    autolayout

--- Psychologist/Psychologist/PsychologistViewController.swift
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class PsychologistViewController: UIViewController {
+class PsychologistViewController
+: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
