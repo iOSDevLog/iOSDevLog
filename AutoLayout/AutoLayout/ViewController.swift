b5f3909bcfb9cbf906698a8498d7285b62209298
jiaxianhua <jiaxianhua@gmail.com>
2015-09-26 23:57:40 +0800

    outlet and image.

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginField: UITextField!
-
     @IBOutlet weak var passwordField: UITextField!
-    
     @IBOutlet weak var passwordLabel: UILabel!
-    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,9 @@ class ViewController: UIViewController {
     private func updateUI() {
         passwordField.secureTextEntry = secure
         passwordLabel.text = secure ? "Secured Password" : "Password"
+        nameLabel.text = loggedInUser?.name
+        companyLabel.text = loggedInUser?.company
+        imageView.image = loggedInUser?.image
     }
     
     @IBAction func toggleSecurity() {
@@ -39,4 +42,6 @@ class ViewController: UIViewController {
     }
     
 }
+
+
 
