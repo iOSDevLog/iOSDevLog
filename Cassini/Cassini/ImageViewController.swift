fa4168f9d1ef9dc71f61f6ef1eb9a4ebd59c7e47
jiaxianhua <jiaxianhua@gmail.com>
2015-09-28 22:40:32 +0800

    imageView.sizeToFit()

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -9,4 +9,15 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
 }
