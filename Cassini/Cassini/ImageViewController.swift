ec91b56f685919be5d47bc7a26b382572806be95
jiaxianhua <jiaxianhua@gmail.com>
2015-09-28 22:48:33 +0800

    imageURL

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let imageData = NSData(contentsOfURL: url)
+            if imageData != nil {
+                image = UIImage(data: imageData!)
+            }
+            else {
+                image = nil
+            }
+        }
+    }
+    
     private var imageView = UIImageView()
     
     private var image: UIImage? {
@@ -24,5 +45,14 @@ class ImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
+    }
+    
+    // for efficiency, we will only actually fetch the image
+    // when we know we are going to be on screen
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
     }
 }
