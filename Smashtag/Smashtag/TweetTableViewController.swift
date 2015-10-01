645f6be5bb4ffb4b1f354c5f01d790212c9448b7
jiaxianhua <jiaxianhua@gmail.com>
2015-10-01 21:26:25 +0800

    change class name

--- Smashtag/Smashtag/TweetTableViewController.swift
+++ Smashtag/Smashtag/TweetTableViewController.swift
@@ -20,11 +20,6 @@ class TweetTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
