a2bc920710a0990f6539e1127c3c742dec401eb4
jiaxianhua <jiaxianhua@gmail.com>
2015-10-01 23:37:17 +0800

    configure Cell

--- Smashtag/Smashtag/TweetTableViewCell.swift
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class TweetTableViewCell: UITableViewCell {
     @IBOutlet weak var tweetProfileImageView: UIImageView!
-    @IBOutlet weak var tweetScreenNameLable: UILabel!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
     @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+            let formatter = NSDateFormatter()
+            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            } else {
+                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            }
+        }
+    }
 }
