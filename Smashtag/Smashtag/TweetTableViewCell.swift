17c1e722f95e1a0ad9d99d64f194f6315b41c09d
jiaxianhua <jiaxianhua@gmail.com>
2015-10-01 22:41:50 +0800

    New TweetTableViewCell

---
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by jiaxianhua on 15/10/1.
+//  Copyright © 2015年 com.jiaxh. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+}
