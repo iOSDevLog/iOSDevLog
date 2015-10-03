99292754da0770d497b60ca25c1cdfc405e3ad0e
jiaxianhua <jiaxianhua@gmail.com>
2015-10-03 17:16:58 +0800

    unwind  ..(must contains detail) why???

--- unwind/unwind/ViewController.swift
+++ unwind/unwind/ViewController.swift
@@ -9,17 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func goBack(segue: UIStoryboardSegue) {
+        print("\(segue)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
