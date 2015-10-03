3b0f9100179f922e7d04f20d10d1892f646dfea1
jiaxianhua <jiaxianhua@gmail.com>
2015-10-03 23:16:28 +0800

    random 10 uiview in top

--- Dropit/Dropit/DropitViewController.swift
+++ Dropit/Dropit/DropitViewController.swift
@@ -9,4 +9,47 @@
 import UIKit
 
 class DropitViewController: UIViewController {
+    @IBOutlet weak var gameView: UIView!
+    
+    var dropPerRow = 10
+    
+    var dropSize: CGSize {
+        let size = gameView.bounds.width / CGFloat(dropPerRow)
+        return CGSize(width: size, height: size)
+    }
+    
+    @IBAction func drop(sender: UITapGestureRecognizer) {
+        drop()
+    }
+    
+    func drop() {
+        var frame = CGRect(origin: CGPointZero, size: dropSize)
+        frame.origin.x = CGFloat.random(dropPerRow) * dropSize.width
+        
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random
+        
+        gameView.addSubview(dropView)
+    }
+}
+
+// MARK: - Extensions
+
+private extension CGFloat {
+    static func random(max: Int) -> CGFloat {
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+private extension UIColor {
+    class var random: UIColor {
+        switch arc4random()%5 {
+        case 0: return UIColor.greenColor()
+        case 1: return UIColor.blueColor()
+        case 2: return UIColor.orangeColor()
+        case 3: return UIColor.redColor()
+        case 4: return UIColor.purpleColor()
+        default: return UIColor.blackColor()
+        }
+    }
 }
