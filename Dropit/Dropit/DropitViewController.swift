c5dfd6c1df1e1225f0befb1b579be13c4037333b
jiaxianhua <jiaxianhua@gmail.com>
2015-10-03 23:01:17 +0800

    set viewcontroller to  "DropitViewController"

---
+++ Dropit/Dropit/DropitViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DropitViewController.swift
+//  Dropit
+//
+//  Created by jiaxianhua on 15/10/3.
+//  Copyright © 2015年 com.jiaxh. All rights reserved.
+//
+
+import UIKit
+
+class DropitViewController: UIViewController {
+}
