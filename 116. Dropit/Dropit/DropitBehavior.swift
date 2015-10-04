e752437d7c393f60afd237f27f2731000994c5b4
jiaxianhua <jiaxianhua@gmail.com>
2015-10-04 18:59:36 +0800

    dropBehavior

--- 116. Dropit/Dropit/DropitBehavior.swift
+++ 116. Dropit/Dropit/DropitBehavior.swift
@@ -18,21 +18,31 @@ class DropitBehavior: UIDynamicBehavior {
         return lazilyCreatedCOllider
         }()
     
+    lazy var dropBehavior: UIDynamicItemBehavior = {
+        let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        lazilyCreatedDropBehavior.allowsRotation = false
+        lazilyCreatedDropBehavior.elasticity = 0.75
+        return lazilyCreatedDropBehavior
+    }()
+    
     override init() {
         super.init()
         addChildBehavior(gravity)
         addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
     }
     
     func addDrop(drop: UIView) {
         dynamicAnimator?.referenceView?.addSubview(drop)
         gravity.addItem(drop)
         collider.addItem(drop)
+        dropBehavior.addItem(drop)
     }
 
     func removeDrop(drop: UIView) {
         gravity.removeItem(drop)
         collider.removeItem(drop)
+        dropBehavior.removeItem(drop)
         drop.removeFromSuperview()
     }
 }
