775b52cac4a77fd908bef8180253f5a0c47d5336
jiaxianhua <jiaxianhua@gmail.com>
2015-10-04 18:48:08 +0800

    new file DropitBehavior

---
+++ 116. Dropit/Dropit/DropitBehavior.swift
@@ -0,0 +1,13 @@
+//
+//  DropitBehavior.swift
+//  Dropit
+//
+//  Created by jiaxianhua on 15/10/4.
+//  Copyright © 2015年 com.jiaxh. All rights reserved.
+//
+
+import UIKit
+
+class DropitBehavior: UIDynamicBehavior {
+
+}
