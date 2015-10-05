eefd89d56719bf93ef1f7e06607878d59aa6458d
jiaxianhua <jiaxianhua@gmail.com>
2015-10-05 16:31:14 +0800

    New Project "Bouncer"

---
+++ Bouncer/Bouncer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bouncer
+//
+//  Created by jiaxianhua on 15/10/5.
+//  Copyright © 2015年 iOSDevLog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
