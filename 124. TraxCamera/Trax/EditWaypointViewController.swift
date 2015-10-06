2cb3ec97322ded24ff587f77a7458799c16942be
jiaxianhua <jiaxianhua@gmail.com>
2015-10-06 18:08:54 +0800

    extension UIImage

--- 124. TraxCamera/Trax/EditWaypointViewController.swift
+++ 124. TraxCamera/Trax/EditWaypointViewController.swift
@@ -79,3 +79,58 @@ class EditWaypointViewController: UIViewController, UITextFieldDelegate {
         return true
     }
 }
+
+extension EditWaypointViewController {
+    func updateImage() {
+        if let url = waypointToEdit?.imageURL {
+            let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { [weak self] in
+                if let imageData = NSData(contentsOfURL: url) {
+                    if url == self?.waypointToEdit?.imageURL {
+                        if let image = UIImage(data: imageData) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self?.imageView.image = image
+                                self?.makeRoomForImage()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // all we do in makeRoomForImage() is adjust our preferredContentSize
+    // we assume that our preferredContentSize is what is currently desired (pre-adjustment)
+    // then we adjust it to make up for any differences in the size of our image view or its container
+    // if our preferredContentSize change can be accomodated, our container will get taller
+    // and more of our image will show
+    // if not, we tried our best to show as much of the image as possible
+    // because we use the entire width of our container view and
+    // show an appropriate height depending on our image's aspect ratio
+    // this is sort of a "quick and dirty" way to do this
+    // in a real application, one would probably want to be more exacting here
+    // (perhaps apply more sophisticated autolayout to the problem)
+    
+    func makeRoomForImage() {
+        var extraHeight: CGFloat = 0
+        if imageView.image?.aspectRatio > 0 {
+            if let width = imageView.superview?.frame.size.width {
+                let height = width / imageView.image!.aspectRatio
+                extraHeight = height - imageView.frame.height
+                imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+            }
+        }
+        else {
+            extraHeight = -imageView.frame.height
+            imageView.frame = CGRectZero
+        }
+        preferredContentSize = CGSize(width: preferredContentSize.width, height: preferredContentSize.height + extraHeight)
+    }
+}
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width / size.height : 0
+    }
+}
+
