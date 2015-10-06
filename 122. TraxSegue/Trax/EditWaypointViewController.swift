2b807c50115acaaea877ddc1310a24feea10b3db
jiaxianhua <jiaxianhua@gmail.com>
2015-10-06 15:36:43 +0800

    removeObserver

--- 122. TraxSegue/Trax/EditWaypointViewController.swift
+++ 122. TraxSegue/Trax/EditWaypointViewController.swift
@@ -29,16 +29,28 @@ class EditWaypointViewController: UIViewController, UITextFieldDelegate {
         observingTextFields()
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let observer = ntfObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(observer)
+        }
+        if let observer = itfObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(observer)
+        }
+    }
+    
+    private var ntfObserver: NSObjectProtocol?
+    private var itfObserver: NSObjectProtocol?
     
     private func observingTextFields() {
         let center = NSNotificationCenter.defaultCenter()
         let queue = NSOperationQueue.mainQueue()
-        center.addObserverForName(UITextFieldTextDidChangeNotification, object: nameTextField, queue: queue) { notification in
+        ntfObserver = center.addObserverForName(UITextFieldTextDidChangeNotification, object: nameTextField, queue: queue) { notification in
             if let waypoint = self.waypointToEdit {
                 waypoint.name = self.nameTextField.text
             }
         }
-        center.addObserverForName(UITextFieldTextDidChangeNotification, object: infoTextField, queue: queue) { notification in
+        itfObserver = center.addObserverForName(UITextFieldTextDidChangeNotification, object: infoTextField, queue: queue) { notification in
             if let waypoint = self.waypointToEdit {
                 waypoint.info = self.infoTextField.text
             }
