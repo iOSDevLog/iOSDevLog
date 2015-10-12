dbff8476ad763e75e8fd46b285f4f8e26cd15f65
jiaxianhua <jiaxianhua@gmail.com>
2015-10-12 21:50:06 +0800

    Copy from "https://github.com/objcio/issue-14-scriptable-apps.git"

---
+++ 135. 14-scriptable-apps/Noteland/NLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLAppDelegate.h
+//  Noteland
+//
+//  Created by Brent Simmons on 6/25/14.
+//  Copyright (c) 2014 Q Branch LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ 135. 14-scriptable-apps/Noteland/NLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  NLAppDelegate.m
+//  Noteland
+//
+//  Created by Brent Simmons on 6/25/14.
+//  Copyright (c) 2014 Q Branch LLC. All rights reserved.
+//
+
+#import "NLAppDelegate.h"
+
+@implementation NLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
