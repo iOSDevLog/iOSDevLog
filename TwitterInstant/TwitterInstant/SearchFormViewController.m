bdfe0de99b1d34d8f86d3628bd876a27ab64e871
jiaxianhua <jiaxianhua@gmail.com>
2015-10-16 19:07:09 +0800

    self.searchText.rac_textSignal

--- TwitterInstant/TwitterInstant/SearchFormViewController.m
+++ TwitterInstant/TwitterInstant/SearchFormViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SearchFormViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface SearchFormViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *searchText;
@@ -18,6 +19,16 @@
     [super viewDidLoad];
     
     self.title = @"Twitter Instant";
+    
+    [[self.searchText.rac_textSignal map:^id(NSString *text) {
+        return [self isValidSearchText:text] ? [UIColor whiteColor] : [UIColor yellowColor];
+    }] subscribeNext:^(UIColor *color) {
+        self.searchText.backgroundColor = color;
+    }];
+}
+
+- (BOOL)isValidSearchText:(NSString *)text {
+    return text.length > 2;
 }
 
 @end
