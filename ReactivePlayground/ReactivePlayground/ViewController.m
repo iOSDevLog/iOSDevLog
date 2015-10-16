070ec8aa1be840f6e62f33645bd22438bed2c637
jiaxianhua <jiaxianhua@gmail.com>
2015-10-16 15:41:48 +0800

    New Project "ReactivePlayground"

---
+++ ReactivePlayground/ReactivePlayground/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ReactivePlayground
+//
+//  Created by JiaXianhua on 15/10/16.
+//  Copyright © 2015年 jiaxianhua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ReactivePlayground/ReactivePlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactivePlayground
+//
+//  Created by JiaXianhua on 15/10/16.
+//  Copyright © 2015年 jiaxianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
