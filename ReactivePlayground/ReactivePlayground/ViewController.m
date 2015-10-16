a1001e35fea7ba8949e2f44a35763a73c1655f3b
jiaxianhua <jiaxianhua@gmail.com>
2015-10-16 17:19:53 +0800

    logic

--- ReactivePlayground/ReactivePlayground/ViewController.m
+++ ReactivePlayground/ReactivePlayground/ViewController.m
@@ -57,8 +57,12 @@
     
     [[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside] flattenMap:^RACStream *(id value) {
         return [self signInSignal];
-    }] subscribeNext:^(id x) {
-        NSLog(@"Sign in result: %@", x);
+    }] subscribeNext:^(NSNumber *signedIn) {
+        BOOL success = [signedIn boolValue];
+        self.signInFailureText.hidden = success;
+        if (success) {
+            [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+        }
     }];
 }
 
