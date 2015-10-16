8833090b5df26768cfe3435ae792d767977a023f
jiaxianhua <jiaxianhua@gmail.com>
2015-10-16 16:38:02 +0800

    filter

--- ReactivePlayground/ReactivePlayground/ViewController.m
+++ ReactivePlayground/ReactivePlayground/ViewController.m
@@ -25,8 +25,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.usernameTextField.rac_textSignal subscribeNext:^(id x) {
-        
+    [[self.usernameTextField.rac_textSignal filter:^BOOL(id value) {
+        NSString *text = value;
+        return text.length > 3;
+    }] subscribeNext:^(id x) {
         NSLog(@"%@", x);
     }];
 }
