54eef5096ea3d34aca1476891b9ed1b97928a623
iosdevlog <iosdevlog@iosdevlog.com>
2015-10-29 20:39:41 +0800

    172. core image explorer

---
+++ 172. core-image-explorer/Core Image Explorer/FilterListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  FilterListViewController.swift
+//  Core Image Explorer
+//
+//  Created by Warren Moore on 1/6/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+import UIKit
+
+class FilterListViewController: UITableViewController {
+    let filters: [(filterName: String, filterDisplayName: String)] = [
+        ("CIBloom", "Bloom"),
+        ("CIColorControls", "Color Controls"),
+        ("CIColorInvert", "Color Invert"),
+        ("CIColorPosterize", "Color Posterize"),
+        ("CIExposureAdjust", "Exposure Adjust"),
+        ("CIGammaAdjust", "Gamma Adjust"),
+        ("CIGaussianBlur", "Gaussian Blur"),
+        ("CIGloom", "Gloom"),
+        ("CIHighlightShadowAdjust", "Highlights and Shadows"),
+        ("CIHueAdjust", "Hue Adjust"),
+        ("CILanczosScaleTransform", "Lanczos Scale Transform"),
+        ("CIMaximumComponent", "Maximum Component"),
+        ("CIMinimumComponent", "Minimum Component"),
+        ("CISepiaTone", "Sepia Tone"),
+        ("CISharpenLuminance", "Sharpen Luminance"),
+        ("CIStraightenFilter", "Straighten"),
+        ("CIUnsharpMask", "Unsharp Mask"),
+        ("CIVibrance", "Vibrance"),
+        ("CIVignette", "Vignette")
+    ]
+
+    override func viewWillAppear(animated: Bool) {
+        UIApplication.sharedApplication().setStatusBarStyle(.Default, animated: animated)
+        navigationController?.navigationBar.barStyle = .Default
+        tabBarController?.tabBar.barStyle = .Default
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let controller = segue.destinationViewController as! FilterDetailViewController
+                controller.filterName = filters[indexPath.row].filterName
+                tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let filterProperties: (filterName: String, filterDisplayName: String) = filters[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("SystemFilterCell", forIndexPath: indexPath) 
+        cell.textLabel!.text = filterProperties.filterDisplayName;
+        return cell
+    }
+}
+
---
+++ 172. core-image-explorer/Core Image Explorer/LabeledSliderView.swift
@@ -0,0 +1,96 @@
+//
+//  LabeledSliderView.swift
+//  Core Image Explorer
+//
+//  Created by Warren Moore on 1/10/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+import UIKit
+
+class LabeledSliderView: UIView {
+    var slider: UISlider!
+    var descriptionLabel: UILabel!
+    var valueLabel: UILabel!
+    var parameter: ScalarFilterParameter!
+    var delegate: ParameterAdjustmentDelegate?
+
+    init(frame: CGRect, parameter: ScalarFilterParameter) {
+        super.init(frame: frame)
+
+        self.parameter = parameter
+
+        addSubviews()
+        addLayoutConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func addSubviews() {
+        slider = UISlider(frame: frame)
+        slider.minimumValue = parameter.minimumValue!
+        slider.maximumValue = parameter.maximumValue!
+        slider.value = parameter.currentValue
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(slider)
+
+        slider.addTarget(self, action: "sliderTouchUpInside:", forControlEvents: .TouchUpInside)
+        slider.addTarget(self, action: "sliderValueDidChange:", forControlEvents: .ValueChanged)
+
+        descriptionLabel = UILabel(frame: frame)
+        descriptionLabel.font = UIFont.boldSystemFontOfSize(14)
+        descriptionLabel.textColor = UIColor(white: 0.9, alpha: 1.0)
+        descriptionLabel.text = parameter.name
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(descriptionLabel)
+
+        valueLabel = UILabel(frame: frame)
+        valueLabel.font = UIFont.systemFontOfSize(14)
+        valueLabel.textColor = UIColor(white: 0.9, alpha: 1.0)
+        valueLabel.textAlignment = .Right
+        valueLabel.text = slider.value.description
+        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(valueLabel)
+    }
+
+    func addLayoutConstraints() {
+        addConstraint(NSLayoutConstraint(item: slider, attribute: .Width, relatedBy: .Equal,
+            toItem: self, attribute: .Width, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: slider, attribute: .Height, relatedBy: .Equal,
+            toItem: self, attribute: .Height, multiplier: 0.5, constant: 0))
+        addConstraint(NSLayoutConstraint(item: slider, attribute: .Bottom, relatedBy: .Equal,
+            toItem: self, attribute: .Bottom, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: slider, attribute: .Leading, relatedBy: .Equal,
+            toItem: self, attribute: .Leading, multiplier: 1, constant: 0))
+
+        addConstraint(NSLayoutConstraint(item: descriptionLabel, attribute: .Top, relatedBy: .Equal,
+            toItem: self, attribute: .Top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: descriptionLabel, attribute: .Height, relatedBy: .Equal,
+            toItem: self, attribute: .Height, multiplier: 0.5, constant: 0))
+        addConstraint(NSLayoutConstraint(item: descriptionLabel, attribute: .Width, relatedBy: .Equal,
+            toItem: self, attribute: .Width, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: descriptionLabel, attribute: .Leading, relatedBy: .Equal,
+            toItem: self, attribute: .Leading, multiplier: 1, constant: 0))
+
+        addConstraint(NSLayoutConstraint(item: valueLabel, attribute: .Top, relatedBy: .Equal,
+            toItem: self, attribute: .Top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: valueLabel, attribute: .Height, relatedBy: .Equal,
+            toItem: self, attribute: .Height, multiplier: 0.5, constant: 0))
+        addConstraint(NSLayoutConstraint(item: valueLabel, attribute: .Width, relatedBy: .Equal,
+            toItem: self, attribute: .Width, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: valueLabel, attribute: .Leading, relatedBy: .Equal,
+            toItem: self, attribute: .Leading, multiplier: 1, constant: 0))
+    }
+
+    func sliderValueDidChange(sender: AnyObject?) {
+        valueLabel.text = String(format: "%0.2f", slider.value)
+    }
+
+    func sliderTouchUpInside(sender: AnyObject?) {
+        if delegate != nil {
+            delegate!.parameterValueDidChange(ScalarFilterParameter(key: parameter.key, value: slider.value))
+        }
+    }
+}
