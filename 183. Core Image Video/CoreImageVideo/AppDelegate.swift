533ec9d26bb409e0050fd817b2fb2d92dcebba0f
iosdevlog <iosdevlog@iosdevlog.com>
2015-11-04 20:48:46 +0800

    183._CoreImageVideo

---
+++ 183. Core Image Video/CoreImageVideo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CoreImageVideo
+//
+//  Created by Chris Eidhof on 03/04/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
