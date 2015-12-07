977ce227da3e284bd4b5ba4544574b3b254c827b
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-08 00:22:40 +0800

    UITableViewController.

---
+++ selfSizingCells/selfSizingCells/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  selfSizingCells
+//
+//  Created by iosdevlog on 15/12/8.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
