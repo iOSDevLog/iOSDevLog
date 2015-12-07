4ba6ae2d6e1d7d6aaa74e7ba240d37be8f6b2a6f
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-08 00:16:41 +0800

    selfSizingCells

---
+++ selfSizingCells/selfSizingCells/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  selfSizingCells
+//
+//  Created by iosdevlog on 15/12/8.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
