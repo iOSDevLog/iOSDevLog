b4d3295eacdebcbfacd2974ba8f297a368b2b006
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-15 23:29:35 +0800

    Getting Started - 1

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by iosdevlog on 15/12/15.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showAlert(sender: UIButton) {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true) { () -> Void in
+            print("You Hit Me!")
+        }
+    }
+    
+}
+
