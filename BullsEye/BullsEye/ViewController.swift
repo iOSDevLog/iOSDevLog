5337281c9e753970c03ace7b999d3a92d596b80a
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-16 00:08:06 +0800

    SliderMoved

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func sliderMoved(sender: UISlider) {
+        print("The value of the slider is now: \(sender.value)")
+    }
+    
     @IBAction func showAlert(sender: UIButton) {
         let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .Alert)
         let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
