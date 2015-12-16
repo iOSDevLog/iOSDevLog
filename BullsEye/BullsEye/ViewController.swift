6f70530b940dcd03af93684bb4c88a2186eda125
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-16 23:17:35 +0800

    forget to updateLables

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         startNewGame()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(sender: UISlider) {
