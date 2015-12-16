7b1fe3eda471d02ccef15a41315047a5c791ceb3
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-16 23:15:37 +0800

    About

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by iosdevlog on 15/12/16.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
