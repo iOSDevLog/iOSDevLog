4f4e72930448180d19d005b587a6d7285832ddb6
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:30:29 +0800

    1 to 5

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -21,7 +21,7 @@ class CheckListViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
