75016325e2f9adf62224c0139beddd39fda7cc93
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:39:28 +0800

    didSelectRowAtIndexPath

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -44,5 +44,9 @@ class CheckListViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
 }
 
