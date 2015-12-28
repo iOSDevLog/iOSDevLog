6937bb7dc1dd7aaf8450d1ca33d9b4d7bdb886ed
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:51:59 +0800

    check

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class CheckListViewController: UITableViewController {
-
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,7 +26,7 @@ class CheckListViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -29,15 +34,15 @@ class CheckListViewController: UITableViewController {
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row % 5 == 0 {
+        if indexPath.row == 0 {
             label.text = "Walk the dog"
-        } else if indexPath.row % 5 == 1 {
+        } else if indexPath.row == 1 {
             label.text = "Brush my teeth"
-        } else if indexPath.row % 5 == 2 {
+        } else if indexPath.row == 2 {
             label.text = "Learn iOS development"
-        } else if indexPath.row % 5 == 3 {
+        } else if indexPath.row == 3 {
             label.text = "Soccer practice"
-        } else if indexPath.row % 5 == 4 {
+        } else if indexPath.row == 4 {
             label.text = "Eat ice cream"
         }
         
@@ -46,8 +51,24 @@ class CheckListViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
             if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-                if cell.accessoryType == .None {
-                        cell.accessoryType = .Checkmark
+                var isChecked = false
+                if indexPath.row == 0 {
+                    row0checked = !row0checked
+                    isChecked = row0checked
+                } else if indexPath.row == 1 {
+                    row1checked = !row1checked
+                    isChecked = row1checked
+                } else if indexPath.row == 2 {
+                    row2checked = !row2checked
+                    isChecked = row2checked
+                } else if indexPath.row == 3 {
+                    row3checked = !row3checked
+                    isChecked = row3checked
+                } else if indexPath.row == 4 {
+                    row4checked = !row4checked
+                    isChecked = row4checked }
+                if isChecked {
+                    cell.accessoryType = .Checkmark
                 } else {
                     cell.accessoryType = .None
                 }
