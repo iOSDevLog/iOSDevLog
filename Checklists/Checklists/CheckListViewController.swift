1aa6b2c08f8be68c4fc4e291aa0c12a3da97e872
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:37:08 +0800

    100 Row

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -21,7 +21,7 @@ class CheckListViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -29,15 +29,15 @@ class CheckListViewController: UITableViewController {
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row == 0 {
+        if indexPath.row % 5 == 0 {
             label.text = "Walk the dog"
-        } else if indexPath.row == 1 {
+        } else if indexPath.row % 5 == 1 {
             label.text = "Brush my teeth"
-        } else if indexPath.row == 2 {
+        } else if indexPath.row % 5 == 2 {
             label.text = "Learn iOS development"
-        } else if indexPath.row == 3 {
+        } else if indexPath.row % 5 == 3 {
             label.text = "Soccer practice"
-        } else if indexPath.row == 4 {
+        } else if indexPath.row % 5 == 4 {
             label.text = "Eat ice cream"
         }
         
