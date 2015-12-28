6b45c51122714b7acdb6eff20566c275f9ecbb9b
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:24:18 +0800

    Rename ViewController

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class CheckListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
