afb21c113bbfcf05b5b972e9b569fc422fe216ca
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:33:44 +0800

    if else

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -27,6 +27,20 @@ class CheckListViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
         
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+        }
+        
         return cell
     }
 
