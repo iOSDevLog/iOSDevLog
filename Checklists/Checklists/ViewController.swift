59cf15f0c8817a7097f228c5e71d471536dacf3c
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-28 21:17:16 +0800

    Create New Project

---
+++ Checklists/Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by iosdevlog on 15/12/28.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
