193455fa70d72ec6fe2a15e614af51a0728ee577
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-29 20:16:36 +0800

    Model

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by iosdevlog on 15/12/29.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
