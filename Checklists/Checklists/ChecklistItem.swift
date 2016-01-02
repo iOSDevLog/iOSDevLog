8de25b10f899a003b580a37d079ad08ec004c42e
iosdevlog <iosdevlog@iosdevlog.com>
2016-01-02 23:36:01 +0800

    class ChecklistItem: NSObject, NSCoding {

--- Checklists/Checklists/ChecklistItem.swift
+++ Checklists/Checklists/ChecklistItem.swift
@@ -8,9 +8,24 @@
 
 import Foundation
 
-class ChecklistItem: NSObject {
+class ChecklistItem: NSObject, NSCoding {
     var text = ""
     var checked = false
+    
+    // MARK: init
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    // MARK: - NSCoding
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(text, forKey: "Text")
+        aCoder.encodeBool(checked, forKey: "Checked")
+    }
     
     func toggleChecked() {
         checked = !checked
