75e5065db44a10bdd2a0c55468fc7cd47721aaf7
iosdevlog <iosdevlog@iosdevlog.com>
2016-01-06 22:43:04 +0800

    CURD

--- Checklists/Checklists/AllListsViewController.swift
+++ Checklists/Checklists/AllListsViewController.swift
@@ -28,21 +28,11 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         list = Checklist(name: "To Do")
         lists.append(list)
         
-        var item = ChecklistItem()
-        item.text = "Item for Birthdays"
-        lists[0].items.append(item)
-        
-        item = ChecklistItem()
-        item.text = "Item for Groceries"
-        lists[1].items.append(item)
-        
-        item = ChecklistItem()
-        item.text = "Item for Cool Apps"
-        lists[2].items.append(item)
-        
-        item = ChecklistItem()
-        item.text = "Item for To Do"
-        lists[3].items.append(item)
+        for list in lists {
+            let item = ChecklistItem()
+            item.text = "Item for \(list.name)"
+            list.items.append(item)
+        }
     }
     
     override func viewDidLoad() {
@@ -138,5 +128,38 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
             }
         }
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - save & load directory
+    func documentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        
+        return paths[0]
+    }
+    
+    func dataFilePath() -> String {
+        return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklists.plist")
+    }
+    
+    // CURD Create / Update / Retrieve / Delete
+    func saveChecklists() {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        archiver.encodeObject(lists, forKey: "Checklists")
+        archiver.finishEncoding()
+        data.writeToFile(dataFilePath(), atomically: true)
+    }
+    
+    func loadChecklists() {
+        // load path
+        let path = dataFilePath()
+        // file exist
+        if NSFileManager.defaultManager().fileExistsAtPath(path) {
+            if let data = NSData(contentsOfFile: path) {
+                let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+                lists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
+                unarchiver.finishDecoding()
+            }
+        }
     }
 }
