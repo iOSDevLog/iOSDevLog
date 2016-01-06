060f6d37e2a13a87ba6ba681a9fc44e8b3d4b079
iosdevlog <iosdevlog@iosdevlog.com>
2016-01-06 22:37:11 +0800

    for list in lists

--- Checklists/Checklists/AllListsViewController.swift
+++ Checklists/Checklists/AllListsViewController.swift
@@ -27,6 +27,12 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         
         list = Checklist(name: "To Do")
         lists.append(list)
+        
+        for list in lists {
+            let item = ChecklistItem()
+            item.text = "Item for \(list.name)"
+            list.items.append(item)
+        }
     }
     
     override func viewDidLoad() {
