bb0c83a5c636114e7cee9f3cc4fe35e3e2820d1c
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-15 22:09:06 +0800

    Dispatch

--- MyLocations/MyLocations/LocationDetailsViewController.swift
+++ MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import Dispatch
 
 class LocationDetailsViewController: UITableViewController {
     // MARK: - outlet
@@ -51,6 +52,13 @@ class LocationDetailsViewController: UITableViewController {
         let hudView = HudView.hudInView(navigationController!.view,
             animated: true)
         hudView.text = "Tagged"
+        
+        let delayInSeconds = 0.6
+        let when = dispatch_time(DISPATCH_TIME_NOW,
+            Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+        dispatch_after(when, dispatch_get_main_queue(), {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        })
     }
     
     @IBAction func cancel() {
