4d50ceea57955c5bb68cc815206036f7040487e4
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-15 22:11:59 +0800

    Refactor

--- MyLocations/MyLocations/LocationDetailsViewController.swift
+++ MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -53,12 +53,9 @@ class LocationDetailsViewController: UITableViewController {
             animated: true)
         hudView.text = "Tagged"
         
-        let delayInSeconds = 0.6
-        let when = dispatch_time(DISPATCH_TIME_NOW,
-            Int64(delayInSeconds * Double(NSEC_PER_SEC)))
-        dispatch_after(when, dispatch_get_main_queue(), {
+        afterDelay(0.6) {
             self.dismissViewControllerAnimated(true, completion: nil)
-        })
+        }
     }
     
     @IBAction func cancel() {
