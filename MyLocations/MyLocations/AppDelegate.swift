40bb1d1dc60af0f525c5a08daeaea85b9649900c
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-16 21:20:12 +0800

    Interface Builder does not allow you to make outlets for your view controllers on the App Delegate.

--- MyLocations/MyLocations/AppDelegate.swift
+++ MyLocations/MyLocations/AppDelegate.swift
@@ -42,6 +42,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let tabBarController = window!.rootViewController as! UITabBarController
+        
+        if let tabBarViewControllers = tabBarController.viewControllers {
+            let currentLocationViewController = tabBarViewControllers[0] as! CurrentLocationViewController
+            
+            currentLocationViewController.managedObjectContext = managedObjectContext
+        }
+        
         return true
     }
 
