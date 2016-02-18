f61b785a11b22260317403dc70cc52b3a38e4498
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-18 21:55:54 +0800

    regionForAnnotations

--- MyLocations/MyLocations/MapViewController.swift
+++ MyLocations/MyLocations/MapViewController.swift
@@ -43,6 +43,43 @@ class MapViewController: UIViewController {
         locations = try! managedObjectContext.executeFetchRequest(fetchRequest) as! [Location]
         mapView.addAnnotations(locations)
     }
+    
+    func regionForAnnotations(annotations: [MKAnnotation]) -> MKCoordinateRegion {
+        var region: MKCoordinateRegion
+        
+        switch annotations.count {
+        case 0:
+            region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
+            
+        case 1:
+                let annotation = annotations[annotations.count - 1]
+            region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 1000, 1000)
+            
+        default:
+                var topLeftCoord = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+            var bottomRightCoord = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+            
+            for annotation in annotations {
+                topLeftCoord.latitude = max(topLeftCoord.latitude, annotation.coordinate.latitude)
+                topLeftCoord.longitude = min(topLeftCoord.longitude, annotation.coordinate.longitude)
+                bottomRightCoord.latitude = min(bottomRightCoord.latitude, annotation.coordinate.latitude)
+                bottomRightCoord.longitude = max(bottomRightCoord.longitude, annotation.coordinate.longitude)
+            }
+            
+            let center = CLLocationCoordinate2D(
+                latitude: topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) / 2,
+                longitude: topLeftCoord.longitude - (topLeftCoord.longitude - bottomRightCoord.longitude) / 2)
+            
+            let extraSpace = 1.1
+            let span = MKCoordinateSpan(
+                latitudeDelta: abs(topLeftCoord.latitude - bottomRightCoord.latitude) * extraSpace,
+                longitudeDelta: abs(topLeftCoord.longitude - bottomRightCoord.longitude) * extraSpace)
+            
+            region = MKCoordinateRegion(center: center, span: span)
+        }
+        
+        return mapView.regionThatFits(region)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
