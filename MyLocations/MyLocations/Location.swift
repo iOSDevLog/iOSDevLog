aece28cc58ebdd0111e3551dbfc4d519a4311f86
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-20 23:40:09 +0800

    removePhotoFile

--- MyLocations/MyLocations/Location.swift
+++ MyLocations/MyLocations/Location.swift
@@ -50,4 +50,18 @@ class Location: NSManagedObject, MKAnnotation {
         userDefaults.synchronize()
         return currentID
     }
+    
+    func removePhotoFile() {
+        if hasPhoto {
+            let path = photoPath
+            let fileManager = NSFileManager.defaultManager()
+            if fileManager.fileExistsAtPath(path) {
+                do {
+                    try fileManager.removeItemAtPath(path)
+                } catch {
+                    print("Error removing file: \(error)")
+                }
+            }
+        }
+    }
 }
