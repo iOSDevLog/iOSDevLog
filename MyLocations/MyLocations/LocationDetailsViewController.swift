cf50a59af76784ef109d9de02de606e77eb92564
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-20 23:33:19 +0800

    edit photo

--- MyLocations/MyLocations/LocationDetailsViewController.swift
+++ MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -53,6 +53,11 @@ class LocationDetailsViewController: UITableViewController {
         
         if let location = locationToEdit {
             title = "Edit Location"
+            if location.hasPhoto {
+                if let image = location.photoImage {
+                    showImage(image)
+                }
+            }
         }
         
         descriptionTextView.text = descriptionText
