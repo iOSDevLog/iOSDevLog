9c35d3666bfd288b2e54294304e381120443b446
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-21 23:15:39 +0800

    viewForHeaderInSection

--- MyLocations/MyLocations/LocationsTableViewController.swift
+++ MyLocations/MyLocations/LocationsTableViewController.swift
@@ -93,6 +93,28 @@ class LocationsTableViewController: UITableViewController {
         }
     }
     
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let labelRect = CGRect(x: 15, y: tableView.sectionHeaderHeight - 14, width: 300, height: 14)
+        let label = UILabel(frame: labelRect)
+        label.font = UIFont.boldSystemFontOfSize(11)
+        
+        label.text = tableView.dataSource!.tableView!(tableView, titleForHeaderInSection: section)
+        
+        label.textColor = UIColor(white: 1.0, alpha: 0.4)
+        label.backgroundColor = UIColor.clearColor()
+        
+        let separatorRect = CGRect(x: 15, y: tableView.sectionHeaderHeight - 0.5, width: tableView.bounds.size.width - 15, height: 0.5)
+        let separator = UIView(frame: separatorRect)
+        separator.backgroundColor = tableView.separatorColor
+        
+        let viewRect = CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.sectionHeaderHeight)
+        let view = UIView(frame: viewRect)
+        view.backgroundColor = UIColor(white: 0, alpha: 0.85)
+        view.addSubview(label)
+        view.addSubview(separator)
+        return view
+    }
+    
     // MARK: - prepareForSegue
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "EditLocation" {
