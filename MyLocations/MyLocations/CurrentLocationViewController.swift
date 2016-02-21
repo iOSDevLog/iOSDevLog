f105c502a1e53cd7868ac06157484902f11ea8b1
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-21 23:58:50 +0800

    animationDidStop

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -345,4 +345,13 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         logoRotator.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
         logoButton.layer.addAnimation(logoRotator, forKey: "logoRotator")
     }
+    
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        containerView.layer.removeAllAnimations()
+        containerView.center.x = view.bounds.size.width / 2
+        containerView.center.y = 40 + containerView.bounds.size.height / 2
+            
+        logoButton.layer.removeAllAnimations()
+        logoButton.removeFromSuperview()
+    }
 }
