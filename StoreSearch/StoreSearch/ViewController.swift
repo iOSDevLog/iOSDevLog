cdbeeb4833193580045ebe330eefcac098d40978
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-22 20:14:56 +0800

    StoreSearch Application

---
+++ StoreSearch/StoreSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by iosdevlog on 16/2/22.
+//  Copyright © 2016年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
