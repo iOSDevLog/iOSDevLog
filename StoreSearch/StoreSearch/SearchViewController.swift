5df6e860ca4928a9b8601b00244d855f5f792782
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-22 20:30:30 +0800

    tableView & searchBar

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    // MARK: - property
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
