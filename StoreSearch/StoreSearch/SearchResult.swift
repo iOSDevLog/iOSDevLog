08b65dfbbaff81c6f89b8356456eb8068e3935b8
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-22 21:00:28 +0800

    SearchResult

---
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by iosdevlog on 16/2/22.
+//  Copyright © 2016年 iosdevlog. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
