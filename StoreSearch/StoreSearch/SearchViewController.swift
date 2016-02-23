58307cb9853b9e8c38e39f75e79b3dc51ff8c4d1
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-23 20:20:19 +0800

    tableView.rowHeight = 80

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -21,6 +21,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.rowHeight = 80;
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
         
         let cellNib = UINib(nibName: "SearchResultCell", bundle: nil)
