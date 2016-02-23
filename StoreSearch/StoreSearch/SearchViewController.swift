d79a9c40a87e847fe17f54a1d10e9957a39f5c0a
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-23 20:47:02 +0800

    searchBar.becomeFirstResponder()

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -36,6 +36,7 @@ class SearchViewController: UIViewController {
         cellNib = UINib(nibName: TableViewCellIdentifiers.nothingFoundCell, bundle: nil)
         tableView.registerNib(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
         
+        searchBar.becomeFirstResponder()
     }
 }
 
