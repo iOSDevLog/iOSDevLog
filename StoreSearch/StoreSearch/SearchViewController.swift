a9bcc99c56ae80195ca66677ab8bbae4913a0461
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-24 23:20:38 +0800

    <

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -55,7 +55,7 @@ extension SearchViewController: UISearchBarDelegate {
             if let jsonString = performStoreRequestWithURL(url) {
                 if let dictionary = parseJSON(jsonString) {
                     searchResults = parseDictionary(dictionary)
-                    searchResults.sortInPlace { $0 < $1 }
+                    searchResults.sortInPlace(<)
                     
                     tableView.reloadData()
                     return
