5ff1ebd0c5700e4c0231c2b2611c0dd28cb53f31
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-27 19:56:16 +0800

    dimmingView.alpha = 0

--- StoreSearch/StoreSearch/DimmingPresentationController.swift
+++ StoreSearch/StoreSearch/DimmingPresentationController.swift
@@ -20,5 +20,12 @@ class DimmingPresentationController: UIPresentationController {
     override func presentationTransitionWillBegin() {
         dimmingView.frame = containerView!.bounds
         containerView!.insertSubview(dimmingView, atIndex: 0)
+    
+        dimmingView.alpha = 0
+        if let transitionCoordinator = presentedViewController.transitionCoordinator() {
+            transitionCoordinator.animateAlongsideTransition({ _ in
+                    self.dimmingView.alpha = 1
+                }, completion: nil)
+        }
     }
 }
