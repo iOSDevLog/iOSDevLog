91473100d7546769d0f901f9db263c427965fe61
iosdevlog <iosdevlog@iosdevlog.com>
2016-02-29 21:45:54 +0800

    autoresizingMask = [.FlexibleWidth , .FlexibleHeight]

--- StoreSearch/StoreSearch/GradientView.swift
+++ StoreSearch/StoreSearch/GradientView.swift
@@ -12,11 +12,13 @@ class GradientView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.clearColor()
+        autoresizingMask = [.FlexibleWidth , .FlexibleHeight]
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         backgroundColor = UIColor.clearColor()
+        autoresizingMask = [.FlexibleWidth , .FlexibleHeight]
     }
     
     override func drawRect(rect: CGRect) {
