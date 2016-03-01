f009df58ee4e7c57f43998a3f298cf62eb227ac6
iosdevlog <iosdevlog@iosdevlog.com>
2016-03-01 20:30:05 +0800

    viewWillLayoutSubviews

--- StoreSearch/StoreSearch/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
+    // MARK: - outlet
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     // MARK: - lifeCycle
     deinit {
         print("deinit \(self)")
     }
-
-
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        
+        pageControl.frame = CGRect(
+            x: 0,
+            y: view.frame.size.height - pageControl.frame.size.height,
+            width: view.frame.size.width,
+            height: pageControl.frame.size.height)
+    }
 }
