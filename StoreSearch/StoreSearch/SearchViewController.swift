0759076297d4d4a6d6f7cfb0a6898aaaadf58bf8
iosdevlog <iosdevlog@iosdevlog.com>
2016-03-03 21:25:02 +0800

    tableview

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -92,36 +92,39 @@ extension SearchViewController: UISearchBarDelegate {
 
 // MARK: - SearchViewController
 extension SearchViewController: UITableViewDataSource {
-    func tableView(tableView: UITableView,
-            numberOfRowsInSection section: Int) -> Int {
-        if search.isLoading {
-            return 1
-        } else if !search.hasSearched {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch search.state {
+        case .NotSearchedYet:
             return 0
-        } else if search.searchResults.count == 0 {
-            return 1
-        } else {
-            return search.searchResults.count
+        case .Loading:
+                return 1
+        case .NoResults:
+                return 1
+        case .Results(let list):
+                return list.count
         }
     }
     
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if search.isLoading {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch search.state {
+        case .NotSearchedYet:
+            fatalError("Should never get here")
+        
+        case .Loading:
             let cell = tableView.dequeueReusableCellWithIdentifier(TableViewCellIdentifiers.loadingCell, forIndexPath:indexPath)
-            
+        
             let spinner = cell.viewWithTag(100) as! UIActivityIndicatorView
             spinner.startAnimating()
-            
             return cell
-        } else if search.searchResults.count == 0 {
+        
+        case .NoResults:
             return tableView.dequeueReusableCellWithIdentifier(TableViewCellIdentifiers.nothingFoundCell,forIndexPath: indexPath)
-        } else {
-            let cell = tableView.dequeueReusableCellWithIdentifier("SearchResultCell", forIndexPath: indexPath) as! SearchResultCell
-            
-            let searchResult = search.searchResults[indexPath.row]
-            cell.cofnigureForSearchResult(searchResult)
-            
+        
+        case .Results(let list):
+            let cell = tableView.dequeueReusableCellWithIdentifier(TableViewCellIdentifiers.searchResultCell,forIndexPath: indexPath) as! SearchResultCell
+        
+            let searchResult = list[indexPath.row]
+            cell.configureForSearchResult(searchResult)
             return cell
         }
     }
@@ -129,10 +132,12 @@ extension SearchViewController: UITableViewDataSource {
     // MARK: - prepareForSegue
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "ShowDetail" {
-            let detailViewController = segue.destinationViewController as! DetailViewController
-            let indexPath = sender as! NSIndexPath
-            let searchResult = search.searchResults[indexPath.row]
-            detailViewController.searchResult = searchResult
+            if case .Results(let list) = search.state {
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                let indexPath = sender as! NSIndexPath
+                let searchResult = list[indexPath.row]
+                detailViewController.searchResult = searchResult
+            }
         }
     }
 }
@@ -146,11 +151,13 @@ extension SearchViewController: UITableViewDelegate {
         
         performSegueWithIdentifier("ShowDetail", sender: indexPath)
     }
-            
-    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        if search.searchResults.count == 0 || search.isLoading {
+    
+    func tableView(tableView: UITableView,
+        willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        switch search.state {
+        case .NotSearchedYet, .Loading, .NoResults:
             return nil
-        } else {
+        case .Results:
             return indexPath
         }
     }
