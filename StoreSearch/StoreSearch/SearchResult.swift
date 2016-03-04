fe22c3cbfac9fb6d4600fb05a8bba5a0f4052ce8
iosdevlog <iosdevlog@iosdevlog.com>
2016-03-04 23:04:27 +0800

    return "(.+)" --> return NSLocalizedString("$1", comment: "Localized kind: $1")

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 iosdevlog. All rights reserved.
 //
 
+import Foundation
+
 func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
     return lhs.name.localizedStandardCompare(rhs.name) == .OrderedAscending
 }
@@ -23,16 +25,16 @@ class SearchResult {
     
     func kindForDisplay() -> String {
         switch kind {
-        case "album": return "Album"
-        case "audiobook": return "Audio Book"
-        case "book": return "Book"
-        case "ebook": return "E-Book"
-        case "feature-movie": return "Movie"
-        case "music-video": return "Music Video"
-        case "podcast": return "Podcast"
-        case "software": return "App"
-        case "song": return "Song"
-        case "tv-episode": return "TV Episode"
+        case "album": return NSLocalizedString("Album", comment: "Localized kind: Album")
+        case "audiobook": return NSLocalizedString("Audio Book", comment: "Localized kind: Audio Book")
+        case "book": return NSLocalizedString("Book", comment: "Localized kind: Book")
+        case "ebook": return NSLocalizedString("E-Book", comment: "Localized kind: E-Book")
+        case "feature-movie": return NSLocalizedString("Movie", comment: "Localized kind: Movie")
+        case "music-video": return NSLocalizedString("Music Video", comment: "Localized kind: Music Video")
+        case "podcast": return NSLocalizedString("Podcast", comment: "Localized kind: Podcast")
+        case "software": return NSLocalizedString("App", comment: "Localized kind: App")
+        case "song": return NSLocalizedString("Song", comment: "Localized kind: Song")
+        case "tv-episode": return NSLocalizedString("TV Episode", comment: "Localized kind: TV Episode")
         default: return kind
         }
     }
